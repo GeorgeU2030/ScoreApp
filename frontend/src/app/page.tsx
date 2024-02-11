f96abcea5ea8971d2d7ad1518de0e7486765d22d
GeorgeU2030 <georgeu2021@gmail.com>
2024-02-10 20:13:37 -0500

    setup_project_frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <div>
+      Hola Mundo
+      <Button color="primary">
+      Button
+    </Button>
+    </div>
+  );
+}
